ffa484c146f3fc6bc845cf8f3dea8cb97fc8a798
tackle <damon.vinciguerra@gmail.com>
2024-09-30 22:01:18 -0600

    trying all of the things

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GravityFormsToGCP
 Copies workouts saved in old.f3nation.com's GravityForm database to the PostgreSQL database in GCP.
+
+See https://cloud.google.com/docs/buildpacks/python#customizing_the_application_entrypoint for gunicorn
--- main.py
+++ main.py
@@ -14,58 +14,55 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(message)s')
 googleLoggingClient = google.cloud.logging.Client()
 googleLoggingClient.setup_logging()
 
-logging.info("Pulling workouts from Google Sheets.")
-google_sheets = GoogleSheets()
-workouts = google_sheets.get_all_workouts()
-logging.info("Pulled " + str(len(workouts)) + ".")
-
-logging.info("Getting input variables.")
-instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
-db_user = os.environ["DB_USER"]
-db_pass = os.environ["DB_PASS"]
-db_name = os.environ["DB_NAME"]
-
-connector = Connector()
-
-def getconn() -> pg8000.dbapi.Connection:
-    conn: pg8000.dbapi.Connection = connector.connect(
-        instance_connection_name,
-        "pg8000",
-        user=db_user,
-        password=db_pass,
-        db=db_name,
-        ip_type=IPTypes.PUBLIC,
-    )
-    return conn
-
-logging.info("Creating PostgreSQL engine.")
-pool = create_engine(
-    "postgresql+pg8000://",
-    creator=getconn
-)
-
-logging.info("Connecting to '" + instance_connection_name + "' with username '" + db_user + "', database '" + db_name + "'.")
-with pool.connect() as db_conn:
-    
-    logging.info("Loading table 'gravityformworkouts'.")
-    table = Table('gravityformworkouts',MetaData(), autoload_with=pool)
-    
-    logging.info("Deleting workouts currently in GCP.")
-    db_conn.execute(table.delete())
-    logging.info("Deletion complete (not yet committed).")
-
-    logging.info("Writing workouts one at a time.")
-    try:
-        for workout in workouts:
-            db_conn.execute(table.insert().values(workout))
-    except Exception as error:
-        logging.error("Could not write data to GCP. Not updating table. Error: " + str(error))
-    else:
-        logging.info("All workouts written. Committing.")
-        db_conn.commit()
-
-logging.info("Done.")
-
-
 def app():
-    exit()
+    logging.info("Pulling workouts from Google Sheets.")
+    google_sheets = GoogleSheets()
+    workouts = google_sheets.get_all_workouts()
+    logging.info("Pulled " + str(len(workouts)) + ".")
+
+    logging.info("Getting input variables.")
+    instance_connection_name = os.environ["INSTANCE_CONNECTION_NAME"]
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+
+    connector = Connector()
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=IPTypes.PUBLIC,
+        )
+        return conn
+
+    logging.info("Creating PostgreSQL engine.")
+    pool = create_engine(
+        "postgresql+pg8000://",
+        creator=getconn
+    )
+
+    logging.info("Connecting to '" + instance_connection_name + "' with username '" + db_user + "', database '" + db_name + "'.")
+    with pool.connect() as db_conn:
+        
+        logging.info("Loading table 'gravityformworkouts'.")
+        table = Table('gravityformworkouts',MetaData(), autoload_with=pool)
+        
+        logging.info("Deleting workouts currently in GCP.")
+        db_conn.execute(table.delete())
+        logging.info("Deletion complete (not yet committed).")
+
+        logging.info("Writing workouts one at a time.")
+        try:
+            for workout in workouts:
+                db_conn.execute(table.insert().values(workout))
+        except Exception as error:
+            logging.error("Could not write data to GCP. Not updating table. Error: " + str(error))
+        else:
+            logging.info("All workouts written. Committing.")
+            db_conn.commit()
+
+    logging.info("Done.")
